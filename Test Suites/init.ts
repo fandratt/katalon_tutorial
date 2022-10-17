2ad7be0b3e4b266079eccd6a78091dd17255397d
Fandratt Muda <fandrattmuda@fanmud.local>
2022-10-17 22:08:43 +0700

    init code

---
+++ Test Suites/init.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>init</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6b1dab0d-f1ae-4c8e-a701-fb89f9bfed47</testSuiteGuid>
+</TestSuiteEntity>
